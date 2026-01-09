8a27a916b742ac78ee10b5cfdf27ddfb6f2e9ee9
Aditi Kulkarni <aditivkulkarni5@gmail.com>
2026-01-09 18:21:49 +0530

    Added files to github

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -5,42 +5,48 @@ import redis
 import socket
 import sys
 import logging
+import threading
 from datetime import datetime
 
 # App Insights
-# TODO: Import required libraries for App Insights
-
-# Logging
-logger = # TODO: Setup logger
-
-# Metrics
-exporter = # TODO: Setup exporter
-
-# Tracing
-tracer = # TODO: Setup tracer
+from applicationinsights.flask.ext import AppInsights
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure.metrics_exporter import MetricsExporter
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.tracer import Tracer
+from opencensus.trace.samplers import ProbabilitySampler
 
 app = Flask(__name__)
 
 # Requests
-middleware = # TODO: Setup flask middleware
+middleware = AppInsights(app)
 
-# Load configurations from environment or config file
+# Load configurations FIRST
 app.config.from_pyfile('config_file.cfg')
+instrumentation_key = os.environ.get('APPINSIGHTS_INSTRUMENTATIONKEY', app.config.get('APPINSIGHTS_INSTRUMENTATIONKEY'))
+button1 = os.environ.get('VOTE1VALUE', app.config.get('VOTE1VALUE'))
+button2 = os.environ.get('VOTE2VALUE', app.config.get('VOTE2VALUE'))
+title = os.environ.get('TITLE', app.config.get('TITLE'))
+show_host = os.environ.get('SHOWHOST', app.config.get('SHOWHOST'))
 
-if ("VOTE1VALUE" in os.environ and os.environ['VOTE1VALUE']):
-    button1 = os.environ['VOTE1VALUE']
-else:
-    button1 = app.config['VOTE1VALUE']
+# Standardize Connection String
+conn_str = f'InstrumentationKey={instrumentation_key}'
 
-if ("VOTE2VALUE" in os.environ and os.environ['VOTE2VALUE']):
-    button2 = os.environ['VOTE2VALUE']
-else:
-    button2 = app.config['VOTE2VALUE']
+# Logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+log_handler = AzureLogHandler(connection_string=conn_str)
+log_handler.lock = threading.Lock()  
+logger.addHandler(log_handler)
 
-if ("TITLE" in os.environ and os.environ['TITLE']):
-    title = os.environ['TITLE']
-else:
-    title = app.config['TITLE']
+# Metrics
+exporter = MetricsExporter(connection_string=f'InstrumentationKey={instrumentation_key}')
+
+# Tracing
+tracer = Tracer(
+    exporter=AzureExporter(connection_string=f'InstrumentationKey={instrumentation_key}'),
+    sampler=ProbabilitySampler(1.0)
+)
 
 # Redis Connection
 r = redis.Redis()
@@ -60,9 +66,12 @@ def index():
 
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
-        # TODO: use tracer object to trace cat vote
+        with tracer.span(name="cat_vote_read"):
+            pass
+
         vote2 = r.get(button2).decode('utf-8')
-        # TODO: use tracer object to trace dog vote
+        with tracer.span(name="dog_vote_read"):
+            pass
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
@@ -74,15 +83,16 @@ def index():
             # Empty table and return results
             r.set(button1,0)
             r.set(button2,0)
+
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
-            # TODO: use logger object to log cat vote
+            logger.info("Cat votes reset", extra=properties)
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
-            # TODO: use logger object to log dog vote
+            logger.info("Dog votes reset", extra=properties)
 
-            return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
+            return render_template("index.html", value1=0, value2=0, button1=button1, button2=button2, title=title)
 
         else:
 
@@ -98,7 +108,8 @@ def index():
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
 if __name__ == "__main__":
-    # TODO: Use the statement below when running locally
+    app.run()
+
     app.run() 
     # TODO: Use the statement below before deployment to VMSS
     # app.run(host='0.0.0.0', threaded=True, debug=True) # remote
